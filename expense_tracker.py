671081c9baac5fe23e8780d2b06f02344202feb4
kirikagayami <kirikagayami@gmail.com>
2026-09-03 06:12:17 +0700

    Added error handling for the input

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,40 +1,45 @@
-expenses = []
-
 num_expenses = int(input("Enter how many expense(s): "))
+if num_expenses == 0:
+    print("Expense(s) cannot be zero.")
+elif num_expenses < 0:
+    print("Expense(s) cannot be negative")
+else:
+    expenses = []
 
-for i in range(num_expenses):
-    description = input("Enter description: ")
-    amount = int(input("Enter expense: "))
+    for i in range(num_expenses):
+        description = input("Enter description: ")
+        amount = int(input("Enter expense: "))
 
-    expense = [description, amount]
-    expenses.append(expense)
-
-
-for i in range(num_expenses):
-    print("Item no ", i+1, ": ", expenses[i][0])
-    print("Item Price: ", expenses[i][1])
-
-total = 0
-for expense in expenses:
-    total += expense[1]
-    
-maximum = 0
-max_desc = ""
-minimum = expenses[0][1]
-min_desc = expenses[0][0]
-for expense in expenses:
-    if expense[1] > maximum:
-        maximum = expense[1]
-        max_desc = expense[0]
-    
-    if expense[1] < minimum:
-        minimum = expense[1]
-        min_desc = expense[0]
-
-average = total/num_expenses
+        expense = [description, amount]
+        expenses.append(expense)
 
 
-print("Total:", total)
-print("Most Pricy Item: ", max_desc, " Price : ", maximum)
-print("Cheapest Item: ", min_desc, " Price : ", minimum)
-print("Average Spent: ", average)
+    for i in range(num_expenses):
+        print("Item no ", i+1, ": ", expenses[i][0])
+        print("Item Price: ", expenses[i][1])
+
+    total = 0
+    for expense in expenses:
+        total += expense[1]
+        
+    maximum = 0
+    max_desc = ""
+    minimum = expenses[0][1]
+    min_desc = expenses[0][0]
+    for expense in expenses:
+        if expense[1] > maximum:
+            maximum = expense[1]
+            max_desc = expense[0]
+        
+        if expense[1] < minimum:
+            minimum = expense[1]
+            min_desc = expense[0]
+
+
+
+
+    print("Total:", total)
+    print("Most Pricy Item: ", max_desc, " Price : ", maximum)
+    print("Cheapest Item: ", min_desc, " Price : ", minimum)
+    average = total/num_expenses
+    print("Average Spent: ", average)
