6c3f11f78882ec5de8f4bf379a8796e98e15ba82
Alif Maulana <kirikagayami@gmail.com>
2026-09-04 22:32:00 +0700

    Update expense_tracker.py
    Improve the menu comments

--- expense_tracker.py
+++ expense_tracker.py
@@ -5,7 +5,7 @@ elif num_expenses < 0:
     print("Expense(s) cannot be negative")
 else:
     expenses = []
-
+# Loop Through the expense list to register a new expense based on num_expense variable
     for i in range(num_expenses):
         description = input("Enter description: ")
         amount = int(input("Enter expense: "))
@@ -16,7 +16,7 @@ else:
         expense = [description, amount]
         expenses.append(expense)
 
-
+# Loop through the expenses to acquire number of item
     for i in range(num_expenses):
         print("Item no ", i+1, ": ", expenses[i][0])
         print("Item Price: ", expenses[i][1])
