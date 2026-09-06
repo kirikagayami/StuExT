94590300f7d0ddb0aa7df03b5ffa81883bee1bf2
kirikagayami <kirikagayami@gmail.com>
2026-09-06 20:00:37 +0700

    Refactor expense tracker into menu system

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,49 +1,58 @@
-num_expenses = int(input("Enter how many expense(s): "))
-if num_expenses == 0:
-    print("Expense(s) cannot be zero.")
-elif num_expenses < 0:
-    print("Expense(s) cannot be negative")
-else:
-    expenses = []
-# Loop Through the expense list to register a new expense based on num_expense variable
-    for i in range(num_expenses):
+option = 0
+expenses = []
+while option != 4:
+    print("=== StuExT ===")
+    print()
+    print("1. Add expenses")
+    print("2. View expenses")
+    print("3. Show statistics")
+    print("4. Exit")
+    print()
+    option = int(input("Choose an option: "))
+    print()
+    
+    if option == 1:
         description = input("Enter description: ")
         amount = int(input("Enter expense: "))
         while amount <= 0:
             print("Please input the valid price.")
             amount = int(input("Enter expense: "))
-
+            
         expense = [description, amount]
+    
         expenses.append(expense)
+        print()
+    
+    if len(expenses) == 0:
+            print ("No data available.")
+    else:
+        if option == 2:
+            for i in range(len(expenses)):
+                print("Item no ", i+1, ": ", expenses[i][0])
+                print("Item Price: ", expenses[i][1])
+                print()
+    
+        if option == 3:
+            total = 0
+            for expense in expenses:
+                total += expense[1]
+                
+            maximum = 0
+            max_desc = ""
+            minimum = expenses[0][1]
+            min_desc = expenses[0][0]
+            for expense in expenses:
+                if expense[1] > maximum:
+                    maximum = expense[1]
+                    max_desc = expense[0]
+                
+                if expense[1] < minimum:
+                    minimum = expense[1]
+                    min_desc = expense[0]
 
-# Loop through the expenses to acquire number of item
-    for i in range(num_expenses):
-        print("Item no ", i+1, ": ", expenses[i][0])
-        print("Item Price: ", expenses[i][1])
-
-# Loop to add total expenses
-    total = 0
-    for expense in expenses:
-        total += expense[1]
-
-    maximum = 0
-    max_desc = ""
-    minimum = expenses[0][1]
-    min_desc = expenses[0][0]
-    for expense in expenses:
-        if expense[1] > maximum:
-            maximum = expense[1]
-            max_desc = expense[0]
-        
-        if expense[1] < minimum:
-            minimum = expense[1]
-            min_desc = expense[0]
-
-
-
-
-    print("Total:", total)
-    print("Most Pricy Item: ", max_desc, " Price : ", maximum)
-    print("Cheapest Item: ", min_desc, " Price : ", minimum)
-    average = total/num_expenses
-    print("Average Spent: ", average)
+            print("Total:", total)
+            print("Most Pricy Item: ", max_desc, " Price : ", maximum)
+            print("Cheapest Item: ", min_desc, " Price : ", minimum)
+            average = total/len(expenses)
+            print("Average Spent: ", average)
+            print()
